1c2020ee973fcc8d195cadb0a9c049738267320c
unknown <kodytran854@gmail.com>
2022-11-23 00:44:29 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# React Cities
+
+1. fork this repository to your github
+2. git clone yourGitHubrepoURL
+3. cd react-cities
+4. npm i
+
+All work will be done in App.js no other components should be created
---
+++ src/App.js
@@ -0,0 +1,44 @@
+// IMPORT useState
+import React,{useState} from "react";
+import './styles.css';
+// IMPORT imageData.js AND STORE IT IN A VARIABLE CALLED imagesArr
+import imagesArr from "./imageData.js"
+export default function App() {
+  // USE useState TO CREATE  [bigImage, setBigImage] 
+  // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
+const [bigImage, setBigImage] = useState(imagesArr[0].img)
+
+  // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
+   // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+  const handleClick = (imageUrl) => {
+    setBigImage(imageUrl)
+    console.log("clicking...")
+   
+  }
+ 
+  // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
+  const images = imagesArr.map((image, index) => {
+    return(
+      <img 
+      src={image.img}
+      alt={image.city}
+      onClick = {() => handleClick(image.img)}
+      className="thumb"
+      key={index} 
+      style = {{ border: bigImage === image.img ? "6px dotted lightgreen" : "" }}
+      />
+    )
+  })
+  // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
+
+  return (
+    <div className="App">
+      <h1>Cities Of The World</h1>
+      <div id="wrapper">
+        <div id="thumbnails">{images}</div>
+        {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
+        <img src={bigImage} id="bigImage" alt='bigImage'/>
+      </div>
+    </div>
+  );
+}
